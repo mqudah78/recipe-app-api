914c21263249c062824f494437b02e292e49a87d
qudah978 <qudah978@gmail.com>
2025-06-27 17:21:51 +0400

    Fix flake8 issues and remove version key from docker-compose

--- app/tests.py
+++ app/tests.py
@@ -12,10 +12,9 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
-
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = calc.subtract(10,15)
-
-        self.assertEqual(res,5)
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
